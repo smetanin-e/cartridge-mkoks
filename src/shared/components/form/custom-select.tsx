0e425ac0eed9bb1e59087c7991b4e78c1558cced
smetanin-e <e91smet15@gmail.com>
2025-09-22 15:42:37 +0300

    разработка универсального компонента для select

--- src/shared/components/form/custom-select.tsx
+++ src/shared/components/form/custom-select.tsx
@@ -1,39 +1,39 @@
 'use client';
 
 import React from 'react';
-import {
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-  Button,
-  Input,
-  Badge,
-} from '@/shared/components/ui';
-import { useCartridgeStore } from '@/shared/store/cartridges';
+import { Popover, PopoverTrigger, PopoverContent, Button, Input } from '@/shared/components/ui';
 import { ChevronDown, X } from 'lucide-react';
 import { cn } from '@/shared/lib';
-import { getStatusBadge } from '../utils';
 import { ClearButton } from '../cleare-button';
-import { CartridgeDTO, CartridgeModelDTO } from '@/shared/services/dto/cartridge-model.dto.';
 
-type departamentType = {
-  name: string;
-  id: number;
+type CustomSelectProps<T> = {
+  items: T[];
+  value: T | null;
+  onChange: (val: T | null) => void;
+  getKey: (item: T) => string | number;
+  getLabel: (item: T) => string; // используется для поиска
+  placeholder?: string;
+  renderItem: (item: T, selected: boolean) => React.ReactNode;
+  renderValue?: (item: T) => React.ReactNode;
 };
-interface Props {
-  className?: string;
-  items: CartridgeDTO[] | CartridgeModelDTO[] | departamentType[];
-}
 
-export const CustomSelect: React.FC<Props> = () => {
-  const items = useCartridgeStore((state) => state.cartridges);
+export function CustomSelect<T>({
+  items,
+  value,
+  onChange,
+  getKey,
+  getLabel,
+  placeholder = 'Select...',
+  renderItem,
+  renderValue,
+}: CustomSelectProps<T>) {
   const [open, setOpen] = React.useState(false);
   const [search, setSearch] = React.useState('');
-  const [value, setValue] = React.useState<string | null>(null);
 
-  const filtered = items.filter((item) => item.number.toLowerCase().includes(search.toLowerCase()));
+  const filtered = items.filter((item) =>
+    getLabel(item).toLowerCase().includes(search.toLowerCase()),
+  );
 
-  const [element, setElement] = React.useState<CartridgeDTO | null>(null);
   return (
     <Popover open={open} onOpenChange={setOpen}>
       <div className='relative'>
@@ -44,39 +44,29 @@ export const CustomSelect: React.FC<Props> = () => {
             aria-expanded={open}
             className='w-full justify-between font-normal relative pr-12'
           >
-            <div className='w-full flex items-center gap-2'>
-              <div>{value ?? 'Select a fruit'}</div>
-              {/* Если тип CartridgeDTO[] то добавляем этот блок */}{' '}
-              {element && (
-                <div className='flex-1 flex justify-between gap-2'>
-                  <Badge variant={'secondary'}>
-                    <strong>{element.model?.model}</strong>
-                  </Badge>
-                  {getStatusBadge(element.status)}
-                </div>
-              )}
+            <div className='flex items-center gap-2 flex-1'>
+              {value ? (renderValue ? renderValue(value) : getLabel(value)) : placeholder}
             </div>
-            {!value && <ChevronDown className=' h-4 w-4 shrink-0 opacity-50' />}
+            {!value && <ChevronDown className='h-4 w-4 shrink-0 opacity-50' />}
           </Button>
         </PopoverTrigger>
         {value && (
           <ClearButton
             onClick={() => {
-              setValue(null);
-              setElement(null);
+              onChange(null);
             }}
           />
         )}
       </div>
 
-      <PopoverContent align='start' className='p-0 relative w-[350px] p-2'>
+      <PopoverContent align='start' className='w-[350px] p-2'>
         <div className='relative'>
           <Input
             placeholder='Поиск...'
             value={search}
             onChange={(e) => setSearch(e.target.value)}
             autoFocus
-          />{' '}
+          />
           {search && <ClearButton onClick={() => setSearch('')} />}
         </div>
 
@@ -84,35 +74,27 @@ export const CustomSelect: React.FC<Props> = () => {
           {filtered.length === 0 && (
             <div className='text-sm text-muted-foreground px-2 py-1.5'>Ничего не найдено</div>
           )}
-          {filtered.map((item) => (
-            <div
-              key={item.id}
-              onClick={() => {
-                setValue(item.number);
-                setElement(item);
-                setOpen(false);
-                setSearch('');
-              }}
-              className={cn(
-                'cursor-pointer px-2 py-1.5 rounded-sm hover:bg-accent',
-                value === item.number && 'bg-accent',
-              )}
-            >
-              <div className='w-full grid grid-cols-[auto_1fr_auto] items-center gap-2'>
-                <div>{item.number}</div>
-                {/* Если тип CartridgeDTO[] то добавляем этот блок */}
-                <>
-                  {' '}
-                  <Badge variant={'outline'}>
-                    <strong> {item.model?.model}</strong>
-                  </Badge>
-                  <div>{getStatusBadge(item.status)}</div>
-                </>
+          {filtered.map((item) => {
+            const selected = value ? getKey(value) === getKey(item) : false;
+            return (
+              <div
+                key={getKey(item)}
+                onClick={() => {
+                  onChange(item);
+                  setOpen(false);
+                  setSearch('');
+                }}
+                className={cn(
+                  'cursor-pointer px-2 py-1.5 rounded-sm hover:bg-accent',
+                  selected && 'bg-accent',
+                )}
+              >
+                {renderItem(item, selected)}
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
       </PopoverContent>
     </Popover>
   );
-};
+}
